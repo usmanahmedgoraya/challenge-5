4747e87dfbb8d6711faba7a160da826037624089
Usmanahmedintern <usman_ahmed_intern@netixsol.com>
2023-12-20 17:37:56 +0500

    API Isssue

--- src/Components/ShoppingProducts.jsx
+++ src/Components/ShoppingProducts.jsx
@@ -26,7 +26,7 @@ const Products = () => {
     
     useEffect(() => {
         TotalProducts()
-        fetchProducts(1)
+        fetchProducts(0)
     }, [])
 
     const handlePaginationNumber = (e) =>{
